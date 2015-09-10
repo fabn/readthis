f13e08243510bfeafba2f0a1a8351c562f33fda6
Fabio Napoleoni <f.napoleoni@gmail.com>
2015-09-10 12:07:31 +0200

    Binary format proof of concept

--- benchmarks/composing.rb
+++ benchmarks/composing.rb
@@ -20,10 +20,25 @@ def compose_c(marshal, compress)
   "R|#{name}#{comp}1|R"
 end
 
+require 'json'
+require 'oj'
+SERIALIZERS = {Marshal => 0x1, JSON => 0x2, Oj => 0x3}.freeze
+COMPRESSED_FLAG = 0x8
+
+# Uses binary flags to store compressions
+def compose_d(marshal, compress)
+  # Example flag byte, uses the following layout to store metadata
+  # | 0000 | 0 | 000 | # => four unused bits, 1 compression bit, 3 bits for serializer, allow up to 8 different marshalers
+  flags = SERIALIZERS[marshal]
+  flags |= COMPRESSED_FLAG if compress
+  [flags].pack('C')
+end
+
 Benchmark.ips do |x|
   x.report('a') { compose_a(Marshal, true) }
   x.report('b') { compose_b(Marshal, true) }
   x.report('c') { compose_c(Marshal, true) }
+  x.report('d') { compose_c(Marshal, true) }
 
   x.compare!
 end
--- benchmarks/parsing.rb
+++ benchmarks/parsing.rb
@@ -4,7 +4,7 @@ def parse_a(string)
   marshal  = string[2, 12].strip
   compress = string[15] == '1'.freeze
 
-  [marshal, compress, string[18..-1]]
+  [Kernel.const_get(marshal), compress, string[18..-1]]
 end
 
 def parse_b(marked)
@@ -14,15 +14,32 @@ def parse_b(marked)
   marshal, c_name, _ = prefix.split('|'.freeze)
 
   compress = c_name == 'true'.freeze
-
+  # Kernel.const_get won't work here because marshal is "Marshal      0"
   [marshal, compress, marked[offset..-1]]
 end
 
-STR = 'R|marshal      0|Rafdlkadfjadfj asdlkfjasdlfkj asdlfkjdasflkjadsflkjadslkjfadslkjfasdlkjfadlskjf laksdjflkajsdflkjadsflkadjsfladskjf laksjflakdjfalsdkjfadlskjf laksdjflkajdsflk j'
+require 'json'
+require 'oj'
+SERIALIZERS = {Marshal => 0x1, JSON => 0x2, Oj => 0x3}.freeze
+DESERIALIZERS = SERIALIZERS.invert.freeze
+COMPRESSED_FLAG = 0x8
+MARSHAL_FLAG = 0x3
+# Example flag for a compressed string serialized with marshal
+BINARY_FLAG = [SERIALIZERS[Marshal] | COMPRESSED_FLAG].pack('C')
+
+STR = 'R|Marshal      0|Rafdlkadfjadfj asdlkfjasdlfkj asdlfkjdasflkjadsflkjadslkjfadslkjfasdlkjfadlskjf laksdjflkajsdflkjadsflkadjsfladskjf laksjflakdjfalsdkjfadlskjf laksdjflkajdsflk j'
+STR2 = BINARY_FLAG << 'Rafdlkadfjadfj asdlkfjasdlfkj asdlfkjdasflkjadsflkjadslkjfadslkjfasdlkjfadlskjf laksdjflkajsdflkjadsflkadjsfladskjf laksjflakdjfalsdkjfadlskjf laksdjflkajdsflk j'
+
+
+def parse_c(binary_string)
+  flags = binary_string[0].unpack('C').first
+  [DESERIALIZERS[flags & MARSHAL_FLAG], flags & COMPRESSED_FLAG, binary_string[1..-1]]
+end
 
 Benchmark.ips do |x|
   x.report('a') { parse_a(STR) }
   x.report('b') { parse_b(STR) }
+  x.report('c') { parse_c(STR2) }
 
   x.compare!
 end
